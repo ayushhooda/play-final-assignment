b2357b9096cd9e7759cd782ff60d0a9672a7a73d
Ayush Hooda <ayush.hooda@knoldus.in>
2018-03-28 18:11:59 +0530

    forgot password linking added

---
+++ app/controllers/ProfileController.scala
@@ -0,0 +1,24 @@
+package controllers
+
+import javax.inject.Inject
+
+import akka.http.scaladsl.model.HttpHeader.ParsingResult.Ok
+import model.UserForms
+import play.api.i18n.I18nSupport
+import play.api.mvc._
+
+class ProfileController @Inject()(cc: ControllerComponents, userForm: UserForms) extends AbstractController(cc) with I18nSupport{
+
+//  def assignments = Action {
+//
+//  }
+
+  def forgotPassword() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.forgotPassword(userForm.forgotPasswordForm))
+  }
+
+  def resetPassword = Action { implicit request: Request[AnyContent] =>
+    Ok("Password successfully changed")
+  }
+
+}
--- app/controllers/SignInController.scala
+++ app/controllers/SignInController.scala
@@ -1,5 +1,17 @@
 package controllers
 
-class SignInController {
+import javax.inject.Inject
+
+import model.UserForms
+import play.api.i18n.I18nSupport
+import play.api.mvc.{AbstractController, AnyContent, ControllerComponents, Request}
+
+class SignInController @Inject()(cc: ControllerComponents, form: UserForms) extends AbstractController(cc) with I18nSupport {
+
+  val message = cc.messagesApi
+
+  def signIn = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.signin(form.signInForm))
+  }
 
 }
